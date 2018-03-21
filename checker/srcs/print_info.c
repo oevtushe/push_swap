d00fdbe3339221e752c2b1f4b3729809fb82209d
oevtushe <casio6290@gmail.com>
2018-03-21 13:03:03 +0200

    Added user input waiting and clear screen after all executed operation.

--- checker/srcs/print_info.c
+++ checker/srcs/print_info.c
@@ -6,7 +6,7 @@
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/15 11:02:27 by oevtushe          #+#    #+#             */
-/*   Updated: 2018/03/15 11:27:10 by oevtushe         ###   ########.fr       */
+/*   Updated: 2018/03/21 12:24:21 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,10 +50,14 @@ static void		op_print(t_list **a, t_list **b, t_operation op, t_excstat stat)
 
 void			print_info(t_list *a, t_list *b, t_opc *opc, t_excstat stat)
 {
-	ft_printf("----- Operation: %s -----\n", opc->op_name);
+	char c;
+
+	ft_printf("\033[2J\033[;;H----- Operation: %s -----\n", opc->op_name);
 	op_print(&a, &b, opc->abbr, stat);
 	while (a || b)
 		print_row(&a, &b);
 	ft_printf("%c%5c\n", '_', '_');
 	ft_printf("%c%5c\n", 'a', 'b');
+	ft_printf("\n%s%s->%s ", BOLD, GREEN, RESET);
+	read(1, &c, 1);
 }
