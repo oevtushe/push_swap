99389419598d2d0a7379188d11b32ae2582d976f
oevtushe <casio6290@gmail.com>
2018-04-19 10:27:12 +0300

    Split file

--- checker/srcs/read_operations.c
+++ checker/srcs/read_operations.c
@@ -1,37 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker_readers.c                                  :+:      :+:    :+:   */
+/*   read_operations.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/09 15:52:42 by oevtushe          #+#    #+#             */
-/*   Updated: 2018/03/09 15:52:44 by oevtushe         ###   ########.fr       */
+/*   Created: 2018/04/19 10:14:29 by oevtushe          #+#    #+#             */
+/*   Updated: 2018/04/19 10:14:31 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
-
-t_list	*read_args_stack(char **data, int size)
-{
-	int		cnt;
-	int		val;
-	t_list	*lst;
-	t_list	*cur;
-
-	cnt = size;
-	lst = NULL;
-	if (data && *data)
-	{
-		while (cnt--)
-		{
-			val = ft_atoi(data[cnt]);
-			cur = ft_lstnew(&val, sizeof(int));
-			ft_lstadd(&lst, cur);
-		}
-	}
-	return (lst);
-}
 
 t_list	*read_operations(int fd)
 {
