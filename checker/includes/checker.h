fe3b0c0480b1dae6a0d76d63f3a0a4311622ed7e
oevtushe <casio6290@gmail.com>
2018-04-19 10:29:52 +0300

    Executors and some generic functions are moved to utils.

--- checker/includes/checker.h
+++ checker/includes/checker.h
@@ -6,55 +6,17 @@
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 12:00:45 by oevtushe          #+#    #+#             */
-/*   Updated: 2018/03/21 13:01:17 by oevtushe         ###   ########.fr       */
+/*   Updated: 2018/04/19 10:09:24 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECKER_H
 # define CHECKER_H
 
-# include <stdlib.h>
-# include "get_next_line.h"
-# include "ft_printf.h"
-
-typedef	enum	e_operation
-{
-	OP_SA,
-	OP_SB,
-	OP_SS,
-	OP_PA,
-	OP_PB,
-	OP_RA,
-	OP_RB,
-	OP_RR,
-	OP_RRA,
-	OP_RRB,
-	OP_RRR
-}				t_operation;
-
-typedef	enum	e_excstat
-{
-	ES_BM,
-	ES_AM,
-	ES_BOTH,
-	ES_NONE
-}				t_excstat;
-
-/*
-** Operation container
-*/
-
-typedef	struct	s_opc
-{
-	t_operation	abbr;
-	char		*op_name;
-}				t_opc;
+#include "utils.h"
 
 void			rebase_op_stack(t_list **op_stack);
-
-int				isvldarg(char **args, int size);
 int				isvldops(t_list *op_stack);
-
 void			print_info(t_list *a, t_list *b, t_opc *opc, t_excstat stat);
 void			print_swap(t_list **a, t_list **b,
 			void (*pf)(t_list **a, t_list **b, char *color));
@@ -63,26 +25,11 @@ void			print_row(t_list **a, t_list **b);
 void			print_crow_b(t_list **a, t_list **b, char *color);
 void			print_crow_a(t_list **a, t_list **b, char *color);
 void			print_crow(t_list **a, t_list **b, char *color);
-
-/*
-** Operation executors
-*/
-
-void			op_executor(t_list **a_stack, t_list **b_stack,
-			t_list *op_stack,
-			void (*print)(t_list*, t_list*, t_opc*, t_excstat));
-int				execute_rev_rotate(t_list **stack);
-t_excstat		execute_rev_rrotate(t_list **a, t_list **b);
-int				execute_rotate(t_list **stack);
-t_excstat		execute_rrotate(t_list **a, t_list **b);
-int				execute_swap(t_list *stack);
-t_excstat		execute_sswap(t_list *a, t_list *b);
-int				execute_push(t_list **fst, t_list **scd);
-
-t_list			*read_args_stack(char **data, int size);
 t_list			*read_operations(int fd);
 void			checker_error(const char *message);
-
 int				st_issorted(t_list *stack);
+void			op_executor(t_list **a_stack, t_list **b_stack,
+		t_list *op_stack, void print(t_list *, t_list *, t_opc *, t_excstat));
+void			free_str_arr(char ***arr, int size);
 
 #endif
--- checker/srcs/op_executor.c
+++ checker/srcs/op_executor.c
@@ -6,38 +6,11 @@
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/09 14:48:56 by oevtushe          #+#    #+#             */
-/*   Updated: 2018/03/21 10:52:04 by oevtushe         ###   ########.fr       */
+/*   Updated: 2018/04/19 10:06:13 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
-
-static t_excstat	op_execute(t_list **a, t_list **b, t_operation op)
-{
-	if (op == OP_SA)
-		return (execute_swap(*a) ? ES_AM : ES_NONE);
-	else if (op == OP_SB)
-		return (execute_swap(*b) ? ES_BM : ES_NONE);
-	else if (op == OP_SS)
-		return (execute_sswap(*a, *b));
-	else if (op == OP_PA)
-		return (execute_push(a, b) ? ES_AM : ES_NONE);
-	else if (op == OP_PB)
-		return (execute_push(b, a) ? ES_BM : ES_NONE);
-	else if (op == OP_RA)
-		return (execute_rotate(a) ? ES_AM : ES_NONE);
-	else if (op == OP_RB)
-		return (execute_rotate(b) ? ES_BM : ES_NONE);
-	else if (op == OP_RR)
-		return (execute_rrotate(a, b));
-	else if (op == OP_RRA)
-		return (execute_rev_rotate(a) ? ES_AM : ES_NONE);
-	else if (op == OP_RRB)
-		return (execute_rev_rotate(b) ? ES_BM : ES_NONE);
-	else if (op == OP_RRR)
-		return (execute_rev_rrotate(a, b));
-	return (0);
-}
 
 void				op_executor(t_list **a_stack, t_list **b_stack,
 		t_list *op_stack, void print(t_list *, t_list *, t_opc *, t_excstat))
