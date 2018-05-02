b9c59596233ca6d60aa2e148fb3049f6fe2e63e1
oevtushe <casio6290@gmail.com>
2018-05-02 16:22:41 +0300

    Some norme.

--- srcs/split_group.c
+++ srcs/split_group.c
@@ -6,7 +6,7 @@
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/20 13:59:09 by oevtushe          #+#    #+#             */
-/*   Updated: 2018/05/02 14:10:50 by oevtushe         ###   ########.fr       */
+/*   Updated: 2018/05/02 15:47:35 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,8 @@ void		split_group_a(t_stack **stack1, t_stack **stack2, int *group_cnt)
 	int		cur_grp;
 
 	cur_grp = (*stack1)->lst->content_size;
-	while ((top_len = top_grp_len((*stack1)->lst)) > 3 && (int)(*stack1)->lst->content_size == cur_grp)
+	while ((top_len = top_grp_len((*stack1)->lst)) > 3 
+			&& (int)(*stack1)->lst->content_size == cur_grp)
 	{
 		++(*group_cnt);
 		split_nmedian_a(stack1, stack2, top_len, *group_cnt);
@@ -51,7 +52,8 @@ void		split_group_a(t_stack **stack1, t_stack **stack2, int *group_cnt)
 	{
 		cur_grp = (*stack2)->lst->content_size;
 		++(*group_cnt);
-		while ((*stack2)->lst && (int)(*stack2)->lst->content_size == cur_grp)
+		while ((*stack2)->lst 
+				&& (int)(*stack2)->lst->content_size == cur_grp)
 		{
 			(*stack2)->lst->content_size = *group_cnt;
 			op_execute_wrp(&(*stack1)->lst, &(*stack2)->lst, OP_PA);
@@ -66,19 +68,21 @@ void		split_group_b(t_stack **stack1, t_stack **stack2, int *group_cnt)
 	int		cur_grp;
 
 	cur_grp = (*stack2)->lst->content_size;
-	while ((top_len = top_grp_len((*stack2)->lst)) > 3 && (int)(*stack2)->lst->content_size == cur_grp)
+	while ((top_len = top_grp_len((*stack2)->lst)) > 3 
+			&& (int)(*stack2)->lst->content_size == cur_grp)
 	{
-		++(*group_cnt);
-		split_nmedian_b(stack1, stack2, top_len, *group_cnt);
+		split_nmedian_b(stack1, stack2, top_len, ++(*group_cnt));
 		if (top_grp_len((*stack1)->lst) > 3)
-			sort_group(stack1, stack2, (int)(*stack2)->lst->content_size, group_cnt);
+			sort_group(stack1, stack2,
+					(int)(*stack2)->lst->content_size, group_cnt);
 	}
 	top_sort(stack1, stack2);
 	if ((top_len = top_grp_len((*stack2)->lst)) > 0)
 	{
 		cur_grp = (*stack2)->lst->content_size;
 		++(*group_cnt);
-		while ((*stack2)->lst && (int)(*stack2)->lst->content_size == cur_grp)
+		while ((*stack2)->lst 
+				&& (int)(*stack2)->lst->content_size == cur_grp)
 		{
 			(*stack2)->lst->content_size = *group_cnt;
 			op_execute_wrp(&(*stack1)->lst, &(*stack2)->lst, OP_PA);
